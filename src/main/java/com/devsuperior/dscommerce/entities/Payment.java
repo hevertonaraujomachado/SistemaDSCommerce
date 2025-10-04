a4a97c2d895c0c781394598137853cc3848c2334
Heverton <heverton@gmail.com>
2025-10-04 09:03:26 -0300

    equals e hashCode

--- src/main/java/com/devsuperior/dscommerce/entities/Payment.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Payment.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_payment")
@@ -59,5 +60,17 @@ public class Payment {
                 ", moment=" + moment +
                 ", order=" + order +
                 '}';
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof Payment payment)) return false;
+
+        return Objects.equals(id, payment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
     }
 }
