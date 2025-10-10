053bdea86b618b2fe5293cae79c3289a85448ecd
Heverton <heverton@gmail.com>
2025-10-09 21:18:03 -0300

    GET product by id

---
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,44 @@
+package com.devsuperior.dscommerce.dto;
+
+import jakarta.persistence.Column;
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrL;
+
+    public ProductDTO(){
+
+    }
+
+    public ProductDTO(Long id, String name, String description, Double price, String imgUrL) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrL = imgUrL;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImgUrL() {
+        return imgUrL;
+    }
+}
