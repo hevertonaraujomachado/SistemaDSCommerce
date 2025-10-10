00989b3cef2ca86f8e5cc8c7b7c9fdb6b5b3ef25
Heverton <heverton@gmail.com>
2025-10-10 13:30:43 -0300

    GET products paged

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -4,15 +4,22 @@ import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository repository;
+
 
     @Transactional(readOnly = true)
     public ProductDTO findById (Long id) {
@@ -24,4 +31,18 @@ public class ProductService {
 
 
     }
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll (Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        ArrayList<Object> dtoPage = new ArrayList<>();
+
+        return result.map(product -> new ProductDTO(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                product.getImgUrL()
+        ));
+    }
 }
+
