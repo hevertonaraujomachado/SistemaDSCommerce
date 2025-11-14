0efe0d2f4a4e5e719b712f07fbd37f59a6d553c6
Heverton <heverton@gmail.com>
2025-11-13 22:55:58 -0300

    Validate self or admin

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -128,19 +128,21 @@ public class User implements UserDetails {
     public Set<Role> getRoles() {
         return roles;
     }
+public boolean hasRole(String roleName) {
+    for (Role role : roles) {
+        if (role.getAuthority().equals(roleName)) {
+            return true;
+        }
+    }
+    return false;
+}
+
     public void addRole(Role role){
         roles.add(role);
 
     }
 
-    public boolean hasRole(String roleName) {
-        for (Role role : roles) {
-            if (role.getAuthority().equals(roleName)) {
-                return true;
-            }
-        }
-        return false;
-    }
+
     @Override
     public String toString() {
         return "User{" +
--- src/main/java/com/devsuperior/dscommerce/services/AuthService.java
+++ src/main/java/com/devsuperior/dscommerce/services/AuthService.java
@@ -10,6 +10,8 @@ public class AuthService {
     @Autowired
     private UserService userService;
 
+
+
     public void validateSelfOrAdmin(long userId) {
         User me = userService.authenticated();
         if (!me.hasRole("ROLE_ADMIN") && !me.getId().equals(userId)) {
