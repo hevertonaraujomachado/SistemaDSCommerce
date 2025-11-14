e5360caf19e444b27a0b25ba703a0aecb1aa1e21
Heverton <heverton@gmail.com>
2025-11-14 07:08:34 -0300

    testado

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -3,10 +3,7 @@ package com.devsuperior.dscommerce.dto;
 
 import com.devsuperior.dscommerce.entities.Category;
 import com.devsuperior.dscommerce.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -23,6 +20,7 @@ public class ProductDTO {
     @NotBlank(message = "Campo requerido")
     private String description;
     @Positive(message = "O preço deve ser positivo")
+    @NotNull(message = "Campo requerido")
     private Double price;
     private String imgUrL;
 @NotEmpty(message = "Deve ter pelo menos uma Categoria")
